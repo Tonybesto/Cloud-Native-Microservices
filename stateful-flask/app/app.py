2c018de2e34de8d0d80d6704c2a95d985eb2a16a
Tonybesto <akojianthony@gmail.com>
2023-11-18 16:58:49 +0100

    update

---
+++ stateful-flask/app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://stateful-flask-user:stateful-flask-password@postgres.postgres.svc.cluster.local:5432/stateful-flask-db'
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200))
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify({'tasks': [{'id': task.id, 'title': task.title, 'description': task.description} for task in tasks]})
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    title = data['title']
+    description = data['description']
+    task = Task(title=title, description=description)
+    db.session.add(task)
+    db.session.commit()
+    return jsonify({'task': {'id': task.id, 'title': task.title, 'description': task.description}})
+
+if __name__ == '__main__':
+app.run(debug=True, host='0.0.0.0', port=5000)
